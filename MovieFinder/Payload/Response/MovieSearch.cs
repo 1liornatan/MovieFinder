b88fb875dbb343aef87dd48251cb1136decd5e6c
1liornatan <1liornatan@gmail.com>
2023-06-04 15:07:17 +0300

    Search Movie Page

---
+++ MovieFinder/Payload/Response/MovieSearch.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieFinder.Payload.Response;
+
+public class MovieSearch
+{
+    public IEnumerable<MovieMetaData> Search { get; set; }
+    public string totalResults { get; set; }
+    public string Response { get; set; }
+}
---
+++ MovieFinder/Services/Response/ServiceResponse.cs
@@ -0,0 +1,19 @@
+﻿using MovieFinder.Payload.Response;
+
+namespace MovieFinder.Services.Response;
+
+public class ServiceResponse<T>
+{
+    public ServiceResponse(T data)
+    {
+        Data = data;
+        Success = data != null ? true:false;
+        Message = data != null ? "Success":"Failure";
+    }
+
+    public T Data { get; set; }
+    public bool Success { get; set; } = true;
+    public string Message { get; set; } = null;
+    public string Error { get; set; } = null;
+    public List<string> ErrorMessages { get; set; } = null;
+}
