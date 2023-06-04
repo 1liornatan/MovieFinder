39190a2b18ac81779ef2abfe1a0881c1f939d44e
1liornatan <1liornatan@gmail.com>
2023-06-05 00:24:02 +0300

    Added Movies List

--- MovieFinder/ClientApp/src/components/Home.js
+++ MovieFinder/ClientApp/src/components/Home.js
@@ -1,90 +1,73 @@
 import React, { Component } from 'react';
 import SearchBar from "./SearchBar";
-
+import {MovieList} from "./MovieList";
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
+    
+    static popularMovies = [
+        {imdbID: 'tt0120338'},
+        {imdbID: 'tt0083866'},
+        {imdbID: 'tt0032138'},
+        {imdbID: 'tt0076759'},
+        {imdbID: 'tt0167260'},
+        {imdbID: 'tt0029583'},
+        {imdbID: 'tt0103064'},
+        {imdbID: 'tt0110357'},
+        {imdbID: 'tt0068646'}
+    ]
 
     constructor(props) {
         super(props);
         this.state = {
-            movies: [],
-            results: 0
+            movies: Home.popularMovies,
+            results: Home.popularMovies.length,
+            popular: true
         }
     }
-
-  
-  onMovieSelect = (movie) => {
-      this.setState({selectedMovie: movie})
-  }
+    
+/*    componentDidMount() {
+        this.loadMovies(this.state.movies, this.state.results).then(r =>
+            console.log('Loaded popular movies successfully'))
+    }*/
+    
+    onMovieSelect = (movie) => {
+        this.setState({selectedMovie: movie})
+    }
     onTermSubmit = async (term) => {
         if (this.state.isSearching) {
             return; // Ignore the submission if a search is already in progress
         }
 
         try {
-            this.setState({ isSearching: true }); // Set the flag to indicate a search is in progress
+            this.setState({isSearching: true}); // Set the flag to indicate a search is in progress
             const response = await fetch(`movie/${term}/1`);
             const data = await response.json();
 
             const movies = data?.data?.search || [];
-            const results = data?.data?.totalResults || 0;
+            const results = data?.data?.results || 0;
             
-            this.setState({ movies: [], results})
-            movies.map(async (item) => {
-                    try {
-                        const movieDataResponse = await fetch(`movie/${item.imdbID}`);
-                        const movieData = await movieDataResponse.json();
-
-                        this.setState((prevState) => {
-                            return {
-                                movies: [...prevState.movies, movieData]
-                            }
-                        });
-                    } catch (e) {
-                        console.log(e)
-                    }
-                }
-            );
+            this.setState({ movies: movies, results: results, popular: false })
         } catch (e) {
             console.log('Encountered an error:', e);
         } finally {
-            this.setState({ isSearching: false }); // Reset the flag once the search is complete
+            this.setState({isSearching: false}); // Reset the flag once the search is complete
         }
     };
+  
 
     render() {
-    return (
-        <div>
-            <h1>Welcome To Movie Finder!</h1>
-            <SearchBar onFormSubmit={this.onTermSubmit}/>
-            <p>Movies:</p>
-            {this.state.movies.length > 0 ? (
-                <ul>
-                    {this.state.movies.map((item, i) => (
-                        <li key={i}>{item.title} - {item.plot}</li>
-                    ))}
-                </ul>
-            ) : (
-                <p>No movies were found.</p>
-            )}
-            {/*<PopularMoviesCatalog/>*/}
-        </div>
-/*      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>*/
-    );
+        return (
+            <div>
+                <h1>Welcome To Movie Finder!</h1>
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <br />
+                {this.state.popular ? (
+                    <p>Popular Movies:</p>
+                ) : (
+                    <p>Search Results:</p>
+                )}
+                <MovieList movies={this.state.movies} results={this.state.results}/>
+            </div>
+        );
   }
 }
